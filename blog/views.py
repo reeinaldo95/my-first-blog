5a3d6a153c32429649dfc83a76645142a2971444
daw <reeinaldo95@gmail.com>
2018-05-24 19:24:52 -0700

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post 
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 def post_list(request):
@@ -10,7 +11,12 @@ def post_list(request):
 
 
 
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
 
+	
+	
 
 	
 
